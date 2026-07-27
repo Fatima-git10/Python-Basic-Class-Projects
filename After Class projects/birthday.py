d8dcb462bbf9bdca5047e5ff0d254a57188b5e17
Fatima-git10 <tahirawan11@gmail.com>
2026-07-27 10:22:43 +0200

    Add files via upload

---
+++ After Class projects/birthday.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+# store five different birthdays 
+# format for date (yyyy, mm, dd)
+
+birthday_1 = date(2013, 7, 13)
+birthday_2 = date(2016, 2, 12)
+birthday_3 = date(2018, 9, 5)
+birthday_4 = date(2020, 12, 25)
+birthday_5 = date(2022, 4, 30)
