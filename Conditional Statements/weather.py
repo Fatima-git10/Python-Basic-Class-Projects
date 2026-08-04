c48ade62704bc3935127d9f7ed2806a5d0c580d2
Fatima-git10 <tahirawan11@gmail.com>
2026-08-04 17:25:36 +0200

    Add files via upload

---
+++ Conditional Statements/weather.py
@@ -0,0 +1,7 @@
+temp =int(input("Enter today's temperature in Celsius:"))
+if temp < 20:
+    print("it is cold today")
+    print("wear a jacket")
+else:
+    print("it is hot today")
+    print("wear a t-shirt")
