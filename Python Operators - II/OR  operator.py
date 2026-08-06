8ba43476c90dd1142f1751d9f1be0b61414587ce
Fatima-git10 <tahirawan11@gmail.com>
2026-08-06 17:16:17 +0200

    Add files via upload

---
+++ Python Operators - II/OR  operator.py
@@ -0,0 +1,11 @@
+day = str(input("Enter the day : "))
+
+day = day.lower()
+
+if day == "saturday" or day == "sunday":
+
+    print("This is weekend")
+
+else:
+
+    print("This is weekday")
---
+++ Python Operators - II/and operatie.py
@@ -0,0 +1,11 @@
+weather = "sunny"
+
+homework = "no"
+
+if weather == "sunny" and homework == "yes":
+
+    print("You can head to the park")
+
+else:
+
+    print("Do not go out")
