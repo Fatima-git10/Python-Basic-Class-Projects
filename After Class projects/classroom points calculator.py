3f05fe2165d89eda4cdbe70aa8c7ef1c2111020f
Fatima-git10 <tahirawan11@gmail.com>
2026-08-08 17:03:54 +0200

    Add files via upload

---
+++ After Class projects/classroom points calculator.py
@@ -0,0 +1,12 @@
+# average calculator
+
+print("enter the score : ")
+score1 = int(input("score1 : "))
+score2 = int(input("score2 : "))
+score3 = int(input("score3 : "))
+score4 = int(input("score4 : "))
+
+total = score1 + score2 + score3 + score4
+average = total / 4
+
+print(average)
