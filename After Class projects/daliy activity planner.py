44f15ed482a037247171026b34c38f1a9548f279
Fatima-git10 <tahirawan11@gmail.com>
2026-08-08 17:18:13 +0200

    Add files via upload

---
+++ After Class projects/daliy activity planner.py
@@ -0,0 +1,17 @@
+temp =int(input("Enter the day of the week (1-7):"))
+if temp == 1:
+    print("It's Monday!")
+elif temp == 2:
+    print("It's Tuesday!")
+elif temp == 3:
+    print("It's Wednesday!")
+elif temp == 4:
+    print("It's Thursday!")
+elif temp == 5:
+    print("It's Friday!")
+elif temp == 6:
+    print("It's Saturday!")
+elif temp == 7:
+    print("It's Sunday!")
+else:
+    print("Invalid input. Please enter a number between 1 and 7.")
