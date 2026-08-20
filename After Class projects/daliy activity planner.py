9f4335b7f2bde8904dea70823be3b5b529c992d9
Fatima-git10 <tahirawan11@gmail.com>
2026-08-20 13:48:59 +0200

    Add files via upload

--- After Class projects/daliy activity planner.py
+++ After Class projects/daliy activity planner.py
@@ -1,17 +1,17 @@
 temp =int(input("Enter the day of the week (1-7):"))
 if temp == 1:
-    print("It's Monday!")
+    print("It's Monday! get ready for the week ahead.")
 elif temp == 2:
-    print("It's Tuesday!")
+    print("It's Tuesday! You can do it!")
 elif temp == 3:
-    print("It's Wednesday!")
+    print("It's Wednesday! Halfway through the week!")
 elif temp == 4:
-    print("It's Thursday!")
+    print("It's Thursday! Keep going!")
 elif temp == 5:
-    print("It's Friday!")
+    print("It's Friday! Almost the end of the week!")
 elif temp == 6:
-    print("It's Saturday!")
+    print("It's Saturday! Enjoy your weekend!")
 elif temp == 7:
-    print("It's Sunday!")
+    print("It's Sunday! Rest and recharge for the week ahead!")
 else:
     print("Invalid input. Please enter a number between 1 and 7.")
